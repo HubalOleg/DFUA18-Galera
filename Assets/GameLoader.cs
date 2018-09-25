5fc3662abda4232051e09def7cfc1ba4290ec87f
Nikita Nesterchuk <nik.nesterchuk@borodar.com>
2018-09-25 14:50:26 +0300

    Implemented welcome screen

---
+++ Assets/GameLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour {
+	
+	//---------------------------------------------------------------------
+	// Internal
+	//---------------------------------------------------------------------
+
+	private const string GAME_SCENE_NAME = "GameScene";
+
+	//---------------------------------------------------------------------
+	// Messages
+	//---------------------------------------------------------------------
+	
+	private void Start () {
+		Invoke("LoadGame", 2f);
+	}
+	
+	//---------------------------------------------------------------------
+	// Helpers
+	//---------------------------------------------------------------------
+
+	private void LoadGame()
+	{
+		SceneManager.LoadSceneAsync(GAME_SCENE_NAME);
+	}
+}
